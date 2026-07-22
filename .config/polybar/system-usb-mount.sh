c63097c30713fe74a70a93a8588e00a93e584aea
seriiserii825 <seriiburduja@gmail.com>
2026-07-22 19:53:24 +0300

    feat: polybar fix disk

--- .config/polybar/system-usb-mount.sh
+++ .config/polybar/system-usb-mount.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
-devices=$(lsblk -Jplno NAME,TYPE,RM,SIZE,MOUNTPOINT,VENDOR)
+devices=$(lsblk -Jplno NAME,TYPE,RM,SIZE,MOUNTPOINT,VENDOR,TRAN,PKNAME)
 # echo "$devices"
+# A partition counts as "pluggable" if its own transport, or its parent
+# disk's transport, is usb. Many USB-SATA enclosures don't set RM=1,
+# so RM alone is not reliable here.
+usb_part_filter='(.blockdevices | map(select(.type=="disk")) | INDEX(.name)) as $disks | .blockdevices[] | select(.type == "part") | . + {ptran: ($disks[.pkname].tran // .tran)} | select(.ptran == "usb")'
 case "$1" in
   --mount)
-    for mount in $(echo "$devices" | jq -r '.blockdevices[]  | select(.type == "part") | select(.rm == "1") | select(.mountpoint == null) | .name'); do
+    for mount in $(echo "$devices" | jq -r "$usb_part_filter"' | select(.mountpoint == null) | .name'); do
       udisksctl mount --no-user-interaction -b "$mount"
 
       mountpoint=$(udisksctl mount --no-user-interaction -b $mount)
@@ -13,29 +17,27 @@ case "$1" in
     done
     ;;
   --unmount)
-    for unmount in $(echo "$devices" | jq -r '.blockdevices[]  | select(.type == "part") | select(.rm == "1") | select(.mountpoint != null) | .name'); do
+    for unmount in $(echo "$devices" | jq -r "$usb_part_filter"' | select(.mountpoint != null) | .name'); do
       udisksctl unmount --no-user-interaction -b "$unmount"
       udisksctl power-off --no-user-interaction -b "$unmount"
     done
     ;;
   *)
     counter=0
-    for mounted in $(echo "$devices" | jq -r '.blockdevices[] | select(.type == "part") | select(.mountpoint != null) | .mountpoint'); do
-      # if mounted != / or /boot
-      if [ "$mounted" != "/" ] && [ "$mounted" != "/boot" ]; then
-        device_size=$(echo "$devices" | jq -r '.blockdevices[] | select(.mountpoint == "'"$mounted"'") | .size')
-        device_name=$(echo "$devices" | jq -r '.blockdevices[] | select(.mountpoint == "'"$mounted"'") | .name')
-        mounted=$(echo "$mounted" | tr -d "/")
-        if [ $counter -eq 0 ]; then
-          space=""
-        else
-          space="   "
-        fi
-        counter=$((counter + 1))
-
-        output="$device_name $device_size"
-        echo "$output" | tr '\n' ' '
+    for mounted in $(echo "$devices" | jq -r "$usb_part_filter"' | select(.mountpoint != null) | .mountpoint'); do
+      device_size=$(echo "$devices" | jq -r '.blockdevices[] | select(.mountpoint == "'"$mounted"'") | .size')
+      device_name=$(echo "$devices" | jq -r '.blockdevices[] | select(.mountpoint == "'"$mounted"'") | .name')
+      used_pct=$(df -P "$mounted" | tail -n1 | awk '{print $5}' | tr -d '%')
+      free_pct=$((100 - used_pct))
+      if [ $counter -eq 0 ]; then
+        space=""
+      else
+        space="   "
       fi
+      counter=$((counter + 1))
+
+      output="${space}${device_name##*/} ${device_size} (${free_pct}% free)"
+      echo "$output" | tr '\n' ' '
     done
     if [ $counter == 0 ]; then
       echo "No USB"
